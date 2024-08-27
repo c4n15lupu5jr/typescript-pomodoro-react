dcdcca8b197b863a6fac78df80d824877a181342
Renan Lobo <renan.lobo08@hotmail.com>
2024-08-27 14:07:25 -0300

    Pomodoro app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { PomodoroTimer } from './components/pomodoro-timer';
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container">
+    <PomodoroTimer pomodoroTime={5} shortRestTime={5} longRestTime={10} cycles={4} />
     </div>
-  );
+    );
 }
 
 export default App;
